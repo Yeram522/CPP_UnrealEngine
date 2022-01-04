5512e17afd32c960293b6acf94aa4a29cde61780
Marey <kamin_s2@g.hongik.ac.kr>
2022-01-04 15:44:44 +0900

    update project

--- testUnrealEngine/Source/testUnrealEngine/testUnrealEngine.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "testUnrealEngine.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, testUnrealEngine, "testUnrealEngine" );
